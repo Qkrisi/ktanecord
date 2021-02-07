46ea22e74b9104121241f1189a80cb07b414667c
Qkrisi <qkrisiyt@gmail.com>
2021-02-07 11:45:15 +0100

    Added .exec and .eval back

---
+++ src/commands/eval.js
@@ -0,0 +1,44 @@
+const profileWhitelist = require('../map.js').profileWhitelist
+
+const clean = text => {
+    if(typeof(text) === "string")
+        return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203))
+    else
+        return text
+}
+
+function censor(censor) {
+  var i = 0;
+
+  return function(key, value) {
+    if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value) 
+      return '[Circular]'; 
+
+    if(i >= 29)
+      return '[Unknown]';
+
+    ++i;
+
+    return value;  
+  }
+}
+
+
+module.exports.run = (client, message, args) => {
+    if(!profileWhitelist.includes(message.author.id)) return
+    try {
+        let code = message.content.slice(5) // .help 5 chars
+        let splitted = code.split("\n")
+        if(splitted[0]==" `js" && splitted[splitted.length-1]=="`")
+        {
+			splitted.shift()
+			splitted.pop()
+			code = splitted.join("\n")
+		}
+        let evaled = eval(code)
+        if(typeof evaled !== "string") evaled = require("util").inspect(evaled)
+        message.channel.send(clean(evaled), {code:"xl"})
+    } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``)
+    }
+}
---
+++ src/commands/exec.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js")
+const fs = require("fs")
+const exec = require('child_process').exec
+const {profileWhitelist} = require("../map.js")
+
+const clean = text => {
+    if(typeof(text) === "string")
+        return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203))
+    else
+        return text
+}
+
+module.exports.run = (client, message, args) => {
+    if (!profileWhitelist.includes(message.author.id)) return    
+    var code = message.content.slice(5)
+    exec(code, (error, stdout, stderr) => {
+        if (!error && stdout) {
+            if (stdout.length > 1000) {
+                stdout = stdout.substr(stdout.length - 999, stdout.length)
+            }
+            message.channel.send("```" + clean(stdout) + "```")
+        } else {
+            message.channel.send("```" + clean(stderr) + "```")
+        }
+    })
+}
