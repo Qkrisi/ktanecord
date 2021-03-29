5599927451fa3a002274c306d9211c490d315972
Qkrisi <qkrisiyt@gmail.com>
2021-03-29 14:12:56 +0200

    Check slash commands in help

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,19 +1,23 @@
+const main = require("../main.js")
 const { embed } = require('../utils')
 const config = require('../../config.json')
 
 module.exports.run = (client, message, args) => {
-	if (args.admin) return message.channel.send(embed.getEmbed("AdminHelp", {
-		SCCMD: `\`${config.token}sc <enable/disable>\``,
-		cooldownCMD: `\`${config.token}setcooldown <seconds>\``,
-		cooldownValue: `🕒 Sets the cooldown value of  \`${config.token}repo --random\` (at least 0)`,
-		manageCMD: `\`${config.token}manage maintainers/bans add/remove <User ID>\``
-	}))
 	if(args.cs) return message.channel.send(embed.getEmbed("CSHelp", {
 		setcs: `\`${config.token}setcs <module>//<value>//<reason>\``,
 		setbosscs: `\`${config.token}setbosscs <module>//<general value>//<ppm value>//<reason>\``,
 		clearcs: `\`${config.token}clearcs <module>\``,
 		comment: `\`${config.token}comment <module>//<comment>\``,
 		getcs: `\`${config.token}getcs <module>\``
+	}))
+	let body = main.getCooldown()
+	let Slash = `Slash commands are currently ${!body.SlashCommands || !body.SlashCommands.includes(message.guild.id) ? "disabled" : "enabled"} on this server.`
+	if (args.admin) return message.channel.send(embed.getEmbed("AdminHelp", {
+		SCCMD: `\`${config.token}sc <enable/disable>\``,
+		cooldownCMD: `\`${config.token}setcooldown <seconds>\``,
+		cooldownValue: `🕒 Sets the cooldown value of  \`${config.token}repo --random\` (at least 0)`,
+		manageCMD: `\`${config.token}manage maintainers/bans add/remove <User ID>\``,
+		SCValue: `Enable or disable slash commands (${Slash})`
 	}))
 	return message.channel.send(embed.getEmbed("Help", {
 		helpCMD: `\`${config.token}help\``,
@@ -27,7 +31,7 @@ module.exports.run = (client, message, args) => {
 		matchDesc: "🔑 Shows the number of modules that matches the specified expression and list up to 10 ones.\n[Full RegEx](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet)\n[Simple RegEx](https://kb.iu.edu/d/ahsf)",
 		contactCMD: `\`${config.token}contact <modder>\``,
 		scoreCMD: `\`${config.token}score <module>\``,
-		Reference: "**-**Modules can be the name, the ID, the periodic symbol or a Regular Expression to the module\n**-**Simple RegEx also has a `#` wildcard here which represents any numeric character (0-9)",
+		Reference: "**-**Modules can be the name, the ID, the periodic symbol or a Regular Expression to the module\n**-**Simple RegEx also has a `#` wildcard here which represents any numeric character (0-9)\nIf slash commands are enabled on a server, you can also select the command and arguments you wish to invoke by typing `/`. "+Slash,
 		b: '\u200b'
 	}))
 }
